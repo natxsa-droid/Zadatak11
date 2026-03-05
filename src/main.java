30e764bb2f613ec343cdba943c95962d72449e3e
natxsa-droid <natxsa@icloud.com>
2026-03-05 20:29:19 +0100

    Zadatak11.iml

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
-import java.io.FileInputStream;
-import java.io.OutputStream;
+import java.util.Scanner;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class main {
     public static void main(String[] args) {
