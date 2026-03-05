9d738bb843ae5d2c9967a9b0dbcbb174a03ac010
natxsa-droid <natxsa@icloud.com>
2026-03-05 20:30:30 +0100

    Zadatak11.iml

--- src/main.java
+++ src/main.java
@@ -4,7 +4,9 @@ import java.io.ObjectInputStream;
 
 public class main {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Unesi putanju datoteke:");
+        String imeDatoteke = scan.nextLine();
 
     }
 
